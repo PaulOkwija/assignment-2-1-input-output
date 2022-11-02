c78280f93e295094ded878713c468f60576ca32b
Alvin Kimbowa <kimbowa2alvin@gmail.com>
2022-11-02 18:57:27 +0300

    Update tests

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,10 +1,10 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "test.h"
-
+#include <cmath>
 
 TEST_CASE( "Tests1", "[T2]" ) {
-    REQUIRE(test_code("Tests/input1.txt") == 17);
-    REQUIRE(test_code("Tests/input2.txt") == 22);
-    REQUIRE(test_code("Tests/input3.txt") == 27);
+    REQUIRE(fabs(test_code("input1.txt") - 975.2295719844358) < 0.01);
+    REQUIRE(fabs(test_code("input2.txt") - 1028.9586776859503) < 0.01);
+    REQUIRE(fabs(test_code("input3.txt") - 979.8339920948616) < 0.01);
 }
