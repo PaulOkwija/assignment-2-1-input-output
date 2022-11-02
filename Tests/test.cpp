c1a94f92d1b2f60bbf850800efff4ea27078fd6b
Alvin Kimbowa <kimbowa2alvin@gmail.com>
2022-11-02 15:36:19 +0300

    Update test.cpp

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -4,7 +4,7 @@
 
 
 TEST_CASE( "Tests1", "[T2]" ) {
-    REQUIRE(test_code("input1.txt") == 17);
-    REQUIRE(test_code("input2.txt") == 22);
-    REQUIRE(test_code("input3.txt") == 27);
+    REQUIRE(test_code("Tests/input1.txt") == 17);
+    REQUIRE(test_code("Tests/input2.txt") == 22);
+    REQUIRE(test_code("Tests/input3.txt") == 27);
 }
